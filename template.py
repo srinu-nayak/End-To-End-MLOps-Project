ff7cc812ea118a9555f302836dfb46e1c9db3203
SRINU-NAYAK <srinukto@gmail.com>
2025-04-18 21:38:34 -0400

    added exception

--- template.py
+++ template.py
@@ -23,6 +23,7 @@ list_of_files = [
     f"{project_name}/pipeline/prediction_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
+    f"{project_name}/exception/__init__.py",
     "app.py",
     "Dockerfile",
     "demo.py",
